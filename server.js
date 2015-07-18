62d743ec39d706f5c2e9fb34d8076419e0163a75
Johnson Yeap <johnson.yeap91@gmail.com>
2015-07-18 16:56:05 +0800

    Source code cleanup

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ var SampleApp = function() {
      */
     self.start = function() {
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
+        self.server.listen(self.port, self.ipaddress, function() {
             console.log('%s: Node server started on %s:%d ...',
                         Date(Date.now() ), self.ipaddress, self.port);
         });
