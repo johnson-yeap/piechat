d32cf770e1993e54e5117c17876fcf4e4b10bd74
Johnson Yeap <johnson.yeap91@gmail.com>
2015-08-08 18:44:16 +0800

    Change redirect path

--- routes.js
+++ routes.js
@@ -68,7 +68,7 @@ module.exports = function(app, passport) {
 
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        successRedirect : '/chat',
+        successRedirect : '/login',
         failureRedirect : '/'
     }));
 
